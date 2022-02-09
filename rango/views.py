dcd5c3dd0c0a9cbecee393645e9b90af33d604a4
Paulius Jankauskas <2578469J@student.gla.ac.uk>
2022-02-09 21:11:25 +0000

    Complete Chapter 10.5 - Create visit cookie

--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,23 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    if(datetime.now() - last_visit_time).days >0:
+        visits = visits+1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
 
 
 def index(request):
@@ -20,11 +36,26 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+
     
-    return render(request, 'rango/index.html', context=context_dict)
+
+    # COOKIES!
+    request.session.set_test_cookie()
+    # Obtain our Response object early so we can add cookie information
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request, response)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 def about(request):
   
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
